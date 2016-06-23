dc376a66e85cc35b8db3bebb46f0344be7a24475
panama69 <github@flynnshome.com>
2016-06-23 13:01:16 -0500

    hackathon server
    changed over to connect to the HTTPS hackathon site
    
    Also added code to show the non public entities for both the workspace
    and shared space.

--- src/octane/Defects.java
+++ src/octane/Defects.java
@@ -33,16 +33,28 @@ import com.google.gson.*;
 // This can be used as a way to unit test Json response never used but looks interesting
 // https://github.com/rest-assured/rest-assured
 public class Defects {
-	public static String OCTANE_SERVER = "10.0.0.35";
-	public static int PORT = 8080;
+	//public static String OCTANE_SERVER = "10.0.0.35";
+	//public static int PORT = 8080;
+	public static String OCTANE_SERVER = "hackathon.almoctane.com";
+	public static int PORT = 443;
 	public static String SHAREDSPACE_ID = "1001";
 	public static String WORKSPACE_ID = "1002";
 	// These values are what Octane gave you when you created the API Access in Octane admin area
+	//for docker image
 	//Client ID: pogo_1p45r0kvo64g7f40j4x2dyjov
 	//Client secret: ?fc6a2678b5dc047S
-	public static String CLIENT_ID = "pogo_1p45r0kvo64g7f40j4x2dyjov";
-	public static String CLIENT_SECRET = "?fc6a2678b5dc047S";
-	public static String BASE_URI = "/api/shared_spaces/"+SHAREDSPACE_ID+"/workspaces/"+WORKSPACE_ID;
+	//public static String CLIENT_ID = "pogo_1p45r0kvo64g7f40j4x2dyjov";
+	//public static String CLIENT_SECRET = "?fc6a2678b5dc047S";
+	
+	//for hackathon https://hackathon.almoctane.com/
+	//hackathon@user Mission-impossible
+	//Client ID: flynn_9q7rewxl8y5kksn72737g01yz
+	//Client secret: @e754f645df745fdR
+	public static String CLIENT_ID = "flynn_9q7rewxl8y5kksn72737g01yz";
+	public static String CLIENT_SECRET = "@e754f645df745fdR";
+
+	public static String BASE_SHAREDSPACES_URI = "/api/shared_spaces/"+SHAREDSPACE_ID;
+	public static String BASE_WORKSPACES_URI = BASE_SHAREDSPACES_URI+"/workspaces/"+WORKSPACE_ID;
 	
 	public static void main(String[] args) throws ParseException,IOException, URISyntaxException {
 		String hpsso_cookie_key = null;
@@ -51,7 +63,7 @@ public class Defects {
 
 		HttpClient httpClient = HttpClientBuilder.create().build();
 		try {
-			HttpHost target = new HttpHost(OCTANE_SERVER, PORT, "http");
+			HttpHost target = new HttpHost(OCTANE_SERVER, PORT, "https");
 
 			// specify the get request
 			HttpPost postRequest = new HttpPost("/authentication/sign_in");
@@ -85,7 +97,7 @@ public class Defects {
 							.equals("LWSSO_COOKIE_KEY") == true) {
 						lwsso_cookie_key = headers[i].getValue().substring(0,
 								headers[i].getValue().indexOf(";"));
-						System.out.println("my lwsso:" + lwsso_cookie_key);
+						//System.out.println("my lwsso:" + lwsso_cookie_key);
 					}
 					if (headers[i].getValue()
 							.substring(0, headers[i].getValue().indexOf("="))
@@ -93,7 +105,7 @@ public class Defects {
 						// parse
 						hpsso_cookie_key = headers[i].getValue().substring(0,
 								headers[i].getValue().indexOf(";"));
-						System.out.println("my hpsso:" + hpsso_cookie_key);
+						//System.out.println("my hpsso:" + hpsso_cookie_key);
 					}
 					if (headers[i].getValue()
 							.substring(0, headers[i].getValue().indexOf("="))
@@ -101,8 +113,7 @@ public class Defects {
 						// parse
 						octane_cookie_key = headers[i].getValue().substring(0,
 								headers[i].getValue().indexOf(";"));
-						System.out.println("my octane user:"
-								+ octane_cookie_key);
+						//System.out.println("my octane user:"+ octane_cookie_key);
 					}
 				}
 
@@ -116,7 +127,7 @@ public class Defects {
 
 			System.out.println("---------------- Get Request ----------------");
 			HttpGet getRequest = setRequestHeaders(hpsso_cookie_key, lwsso_cookie_key);
-			getRequest.setURI(URI.create (BASE_URI+ "/defects?fields=logical_name,name"));	//&order_by=id&limit=2"
+			getRequest.setURI(URI.create (BASE_WORKSPACES_URI+ "/defects?fields=logical_name,name"));	//&order_by=id&limit=2"
 			
 			System.out.println("executing request to " + target
 					+ getRequest.getURI());
@@ -145,8 +156,40 @@ public class Defects {
 					System.out.println("Element: '"+elementName+"' is not an array");
 			}
 			
-			s = useMetaData(httpClient, target, getRequest, hpsso_cookie_key, lwsso_cookie_key);
+			System.out.println("\r-------------- Available SHAREDSPACES Rest Entities -------------");
+			getRequest.setURI(URI.create (BASE_SHAREDSPACES_URI+ "/metadata/entities"));
+			s = getMetaData(httpClient, target, getRequest, hpsso_cookie_key, lwsso_cookie_key);
 
+			jo = new JSONObject(s);
+			for (int x=0; x< jo.getInt("total_count"); x++){
+				//System.out.println(jo.getJSONArray("data").getJSONObject(x).get("features").toString());
+				JSONArray entity1 = jo.getJSONArray("data").getJSONObject(x).getJSONArray("features");
+				for (int y=0; y<entity1.length(); y++){
+					try {
+						//String.format("%4d", i * j);
+						System.out.println(String.format("%30s",entity1.getJSONObject(y).get("url"))+"\t"+entity1.getJSONObject(y).get("methods"));
+					} catch (JSONException e){
+						
+					}
+				}
+			}
+			System.out.println("\r-------------- Available WORKSPACES Rest Entities -------------");
+			getRequest.setURI(URI.create (BASE_WORKSPACES_URI+ "/metadata/entities"));
+			s = getMetaData(httpClient, target, getRequest, hpsso_cookie_key, lwsso_cookie_key);
+
+			jo = new JSONObject(s);
+			for (int x=0; x< jo.getInt("total_count"); x++){
+				//System.out.println(jo.getJSONArray("data").getJSONObject(x).get("features").toString());
+				JSONArray entity1 = jo.getJSONArray("data").getJSONObject(x).getJSONArray("features");
+				for (int y=0; y<entity1.length(); y++){
+					try {
+						//String.format("%4d", i * j);
+						System.out.println(String.format("%30s",entity1.getJSONObject(y).get("url"))+"\t"+entity1.getJSONObject(y).get("methods"));
+					} catch (JSONException e){
+						
+					}
+				}
+			}
 		} finally {
 		}
 	}
@@ -154,7 +197,18 @@ public class Defects {
 		// print out the element names that occur in the JSONObject
 		String[] elementNames = JSONObject.getNames(jo);
 		for (String elementName:elementNames){
-			System.out.println("Element Name: "+ elementName);
+			System.out.print("Element Name: "+ elementName);
+			//JSONObject jObj = new JSONObject(jString);
+			Object aObj = jo.get(elementName);
+			if(aObj instanceof Integer){
+			    System.out.println(" "+jo.getInt(elementName));
+			} else if (aObj instanceof String){
+				System.out.println(" "+jo.getString(elementName));
+			} else if (aObj instanceof JSONArray){
+				System.out.println(" JSON Array");
+			} else {
+				System.out.println(" "+jo.getBoolean(elementName));
+			}
 		}
 	}
 	
@@ -233,17 +287,13 @@ public class Defects {
 		return getRequest;
 	}
 	
-	public static String useMetaData (HttpClient httpClient, HttpHost target, HttpGet getRequest, String hpsso_cookie_key, String lwsso_cookie_key) throws ParseException, IOException, URISyntaxException{
+	public static String getMetaData (HttpClient httpClient, HttpHost target, HttpGet getRequest, String hpsso_cookie_key, String lwsso_cookie_key) throws ParseException, IOException, URISyntaxException{
 		System.out.println("---------------- Get Request ----------------");
- 
-		//URI myUri = URI.create(BASE_URI+URLEncoder.encode("/metadata/entities", "UTF8")); //?query="id EQ 1001", "UTF8"));
-		//URI myUri = URI.create(BASE_URI+"/metadata/entities");
-		//URI myUri = URI.create(BASE_URI+"/user_tags");
 
-		String q = "\"name EQ ^defect^\"";
-		getRequest.setURI(URI.create(BASE_URI+"/metadata/entities?query="+URLEncoder.encode(q, "UTF-8")));
+		//String q = "\"name EQ ^defect^\"";
+		//getRequest.setURI(URI.create(BASE_URI+"/metadata/entities?query="+URLEncoder.encode(q, "UTF-8")));
 		//getRequest.setURI(URI.create(BASE_URI+"/metadata/entities"));
-
+		//getRequest.setURI(URI.create());
 		System.out.println("executing request to "
 				+ getRequest.getURI());
 
@@ -255,6 +305,7 @@ public class Defects {
 		if (entity != null) {
 			System.out.println("Json Response: " + s);// EntityUtils.toString(entity));
 		}
+		System.out.println("-------------- End Get Request --------------");
 		
 		return s;
 	}
